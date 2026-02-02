5fa21c1718cbbe58c6b1ced62f0a641208e27125
Rohit Sisodiya <rohitsisodiya2503@gmail.com>
2026-02-02 11:11:03 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (91.16%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.cpp
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -2,21 +2,16 @@ class Solution {
 public:
     int lengthOfLastWord(string s) {
 
-        string word = "";
-        for( int i = 0 ; i < s.length() ; i++){
-            
-            if( s[i] == ' '){
-                if( (i+1) < s.length() && s[i+1] != ' ')
-                word = "" ;
-            }
-            else word = word + s[i] ;
-            
+        int i = s.size()-1;
+        int count = 0;
+        while (i >= 0 && s[i] == ' ') {
+            i--;
         }
-        int count = 0 ;
-        for( int i = 0 ; i < word.length() ; i++){
-            count++ ;
+        while (i >= 0 && s[i] != ' ') {
+
+            count++;
+            i--;
         }
-        return count ;
-        
+        return count;
     }
 };
