c114beefd66b63f09bfc45c7e89199cba76824be
Rohit Sisodiya <rohitsisodiya2503@gmail.com>
2026-02-03 15:59:29 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (96.79%) - LeetSync

--- 520-detect-capital/detect-capital.cpp
+++ 520-detect-capital/detect-capital.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     bool detectCapitalUse(string word) {
-        int upper = 0, lower = 0;
-
-        for(char ch : word) {
-            if(ch >= 'A' && ch <= 'Z') upper++;
-            else lower++;
-        }
-        if(upper == word.length()) return true;
-
-        if(lower == word.length()) return true;
-
-        if(word[0] >= 'A' && word[0] <= 'Z' && lower == word.length() - 1) return true;
         
-        return false;
+            int upper = 0 ;
+            int lower = 0 ;
+            for( auto ch : word){
+
+                    if( ch >= 'A' && ch <= 'Z') upper++ ;
+                    else lower++ ;
+            }
+            if( upper == word.length()) return true ;
+            if( lower == word.length()) return true ;
+            if( word[0] >= 'A' && word [0] <= 'Z' && lower == word.length()-1) return true ;
+            else return false ;
     }
 };
