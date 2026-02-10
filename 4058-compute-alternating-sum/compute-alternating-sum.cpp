d81bebf7e57fd13058a7e97ca85a0173560f9a8b
Rohit Sisodiya <rohitsisodiya2503@gmail.com>
2026-02-10 14:50:37 +0530

    Time: 0 ms (100.00%) | Memory: 32.5 MB (46.42%) - LeetSync

--- 4058-compute-alternating-sum/compute-alternating-sum.cpp
+++ 4058-compute-alternating-sum/compute-alternating-sum.cpp
@@ -5,10 +5,8 @@ public:
             int sum = 0 ;
             for( int i = 0 ; i < nums.size() ; i++){
 
-                    if( i % 2 == 0 ){
-                        sum += nums[i] ;
-                    }
-                    else sum -= nums[i] ;
+                if( i % 2 == 0 ) sum += nums[i] ;
+                else sum -= nums[i] ;
             }
             return sum ;
     }
