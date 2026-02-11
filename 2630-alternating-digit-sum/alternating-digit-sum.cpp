2f5528d52f377d4d461c209fc62534bf5fad367e
Rohit Sisodiya <rohitsisodiya2503@gmail.com>
2026-02-11 15:40:11 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (79.75%) - LeetSync

--- 2630-alternating-digit-sum/alternating-digit-sum.cpp
+++ 2630-alternating-digit-sum/alternating-digit-sum.cpp
@@ -2,20 +2,20 @@ class Solution {
 public:
     int alternateDigitSum(int n) {
 
-        int rev = 0;
+        int reverse = 0;
         while (n) {
-            rev = rev * 10 + (n % 10);
+            reverse = reverse * 10 + (n % 10);
             n /= 10;
         }
         int sum = 0;
         int i = 0;
-        while (rev) {
-            int digit = rev % 10;
+        while (reverse) {
+            int digit = reverse % 10;
             if (i % 2 == 0)
                 sum += digit;
             else
                 sum += (-1 * digit);
-            rev /= 10;
+            reverse /= 10;
             i++;
         }
         return sum;
