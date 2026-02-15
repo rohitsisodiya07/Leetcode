e137db3f1e7b9a9c6fe416f6a2cc060dcf1f6660
Rohit Sisodiya <rohitsisodiya2503@gmail.com>
2026-02-15 21:17:34 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (64.31%) - LeetSync

--- 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.cpp
+++ 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     char repeatedCharacter(string s) {
-        map <char, int> m ;
-        for( auto ch : s){
-            m[ch]++ ;
-            if( m[ch] == 2 ){
-                return ch ;
+
+        map<char, int> m;
+        for (auto ch : s) {
+
+            m[ch]++;
+            if (m[ch] == 2) {
+                return ch;
             }
         }
-        return 0 ;
+        return 0;
     }
 };
