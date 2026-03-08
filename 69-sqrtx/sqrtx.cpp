e3cf1f67b01127c99af9d45fb4a7f8475f188eca
Rohit Sisodiya <rohitsisodiya2503@gmail.com>
2026-03-08 11:29:39 +0530

    Time: 2 ms (20.50%) | Memory: 8.5 MB (85.37%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -1,8 +1,24 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        
-        double square = floor(sqrt(x)) ;
-        return square ;
+
+        long long left = 1;
+        long long right = x;
+        long long ans = 0;
+
+        while (left <= right) {
+
+            long long mid = left + (right - left) / 2;
+            if (mid * mid == x)
+                return mid;
+            else if (mid * mid < x) {
+                ans = mid;
+                left = mid + 1;
+            } else {
+
+                right = mid - 1;
+            }
+        }
+        return ans ;
     }
 };
