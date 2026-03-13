2c51dc4159a537ef38e01f0efb80b3b67d4a8a5a
Rohit Sisodiya <rohitsisodiya2503@gmail.com>
2026-03-13 19:41:55 +0530

    Time: 0 ms (100.00%) | Memory: 24.8 MB (5.12%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        
-            vector <int> ans;
-            for( auto ch : nums){
-                if( ch != 0) ans.push_back(ch) ;
+        vector<int> ans ;
+        for( auto ch : nums){
+            if(ch != 0){
+                ans.push_back(ch) ;
             }
-            int i = 0 ;
-            for( auto ch : ans){
-
-                nums[i] = ch ;
-                i++;
-            }
-            while( i < nums.size() ){
-
-                nums[i] = 0 ;
-                i++ ;
-            }
-
+        }
+        int i = 0 ;
+        while( i < ans.size()){
+            nums[i] = ans[i] ;
+            i++;
+        }
+        while( i < nums.size()){
+            nums[i] = 0 ;
+            i++;
+        }
     }
 };
